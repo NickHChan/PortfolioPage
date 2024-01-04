96a527331cf7010d5b14c119445ef8a29ac5680f
Nick Chan <nickharuchan@gmail.com>
2024-01-03 21:24:45 -0500

    changed contactme to be tsx, added onclick function that gives user a notifcation that they copied my e-mail address when they click on it. included a use effect that clears a timer each time they click to copy

---
+++ src/app/components/contactme/ContactMe.tsx
@@ -0,0 +1,64 @@
+'use client'
+import React, {useState, useEffect} from 'react'
+import styles from './contactMe.module.css'
+import Link from 'next/link'
+import { motion } from 'framer-motion'
+
+export default function ContactMe() {
+const [copied, setCopied] = useState<boolean>(false);
+
+const copiedPopUp = {
+  show:{
+    opacity: 1,
+    transition:{
+      duration: 0.5
+    },
+    display: "block"
+  },
+  hidden:{
+    opacity: 0
+  },
+    transitionEnd:{
+      display:"none"
+    }
+  }
+
+  useEffect(()=>{
+    const timer = setInterval(()=>{
+      setCopied(false)
+    }, 1000)
+    return( ()=>{
+      clearInterval(timer)
+    }
+    )
+  },[copied])
+
+  const handleOnClick = (e:React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    navigator.clipboard.writeText(e.currentTarget.value)
+    setCopied(true)
+  }
+
+  return (
+    <div className={styles.contactMeBody}>
+        <h4>Contact Me</h4>
+        <section className={styles.contactInfo}>
+            <div>Image Box</div>
+            <aside>
+                <h5>Nicholas Chan</h5>
+                <motion.div
+                initial='hidden'
+                animate={copied ? 'show' : 'hidden'}
+                variants={copiedPopUp}
+                >
+                  <div className={styles.popupBG}>
+                    <p>Copied!</p>
+                  </div>
+                </motion.div>
+                <p>Email: <button className={styles.button}  value='Nickharuchan@gmail.com'onClick={handleOnClick}>Nickharuchan@gmail.com</button></p>
+                <p>Github: <Link href='https://github.com/NickHChan' rel='noopener noreferral' target='_blank' className={styles.link}>github.com/NickHChan</Link></p>
+            </aside>
+        </section>
+    </div>
+  )
+}
