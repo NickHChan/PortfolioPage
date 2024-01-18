d77a57f9abf81845654ca0194ffceeea57917675
Nick Chan <nickharuchan@gmail.com>
2024-01-18 13:48:55 -0500

    changed montserat font to roboto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,15 +5,13 @@ import {ScrollProvider} from './helper/scrollToContext'
 import {ThemeProvider} from './helper/themeContext'
 import Footer from './components/footer/Footer'
 import { Analytics } from '@vercel/analytics/react'
-import { Montserrat} from 'next/font/google'
+import { Montserrat, Roboto} from 'next/font/google'
 import { Sorts_Mill_Goudy } from 'next/font/google'
 
-
- const montserrat = Montserrat({
-  subsets: ["latin"],
-  weight: ["400","600","800"],
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ["400", "700", "900"]
 })
-
 const sortsMillGoudy = Sorts_Mill_Goudy({
   subsets: ["latin"],
   weight: ["400"]
@@ -32,7 +30,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${montserrat.className} ${sortsMillGoudy.className}`}>
+      <body className={`${roboto.className}${sortsMillGoudy.className}`}>
         <ThemeProvider>
         <ScrollProvider>
           <NavBar />
