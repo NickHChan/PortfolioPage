50a8ebd33d708ba978e0b632df3dea43aa574fb5
Nick Chan <nickharuchan@gmail.com>
2024-01-22 11:56:46 -0500

    Restructed Mainarticle and NightArticle semantics

--- src/app/components/mainArticle/MainArticle.tsx
+++ src/app/components/mainArticle/MainArticle.tsx
@@ -4,11 +4,10 @@ import styles from './MainArticle.module.css'
 export default function MainArticle() {
   return (
     <section className={styles.main}>
-      <p>Hello! I am a web developer with a distinctive background in healthcare and management.<br/><br/> 
-          My journey into technology has been self-driven, fueled by an unwavering passion for learning and appreciation for the ever change of new technologies.<br/><br/>
-          Currently, I am honing my skills in front-end development, with a clear goal to evolve into a full-stack developer.<br/><br/>
-          Immersed in the world of computer science, I am eager to integrate this knowledge and my enthusiasm for continuous learning into a successful and rewarding career.
-      </p>
+      <p>Hello! I am a web developer with a distinctive background in healthcare and management.</p>
+      <p>My journey into technology has been self-driven, fueled by an unwavering passion for learning and appreciation for the ever change of new technologies.</p>
+      <p>Currently, I am honing my skills in front-end development, with a clear goal to evolve into a full-stack developer.</p>
+      <p>Immersed in the world of computer science, I am eager to integrate this knowledge and my enthusiasm for continuous learning into a successful and rewarding career.</p>
     </section>
 
   )
--- src/app/components/nightArticle/NightArticle.tsx
+++ src/app/components/nightArticle/NightArticle.tsx
@@ -4,11 +4,10 @@ import styles from './NightArticle.module.css'
 export default function NightArticle() {
   return (
     <section className={styles.main}>
-      <p>The best way I find relaxation is through gaming, a hobby I&apos;ve cherished since childhood.<br/><br/>
-         My gaming journey began with one of Blizzard&apos;s biggest games, StarCraft, and it has started my addiction ever since.<br/><br/>
-         In times when gaming isn&apos;t an option, I turn to music for relaxation, particularly enjoying the songs by Infected Mushroom, one of my favorite musical duos.<br/><br/>
-         And if you ever need someone to join you in a game, don&apos;t hesitate to give me a shout!<br/><br/>
-      </p>
+      <p>The best way I find relaxation is through gaming, a hobby I&apos;ve cherished since childhood.</p>
+      <p>My gaming journey began with one of Blizzard&apos;s biggest games, StarCraft, and it has started my addiction ever since.</p>
+      <p>In times when gaming isn&apos;t an option, I turn to music for relaxation, particularly enjoying the songs by Infected Mushroom, one of my favorite musical duos.</p>
+      <p>And if you ever need someone to join you in a game, don&apos;t hesitate to give me a shout!</p>
     </section>
 
   )
