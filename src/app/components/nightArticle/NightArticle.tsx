13b4130b10f348e8017a8a73b5706271f8731351
Nick Chan <nickharuchan@gmail.com>
2024-02-09 00:31:53 -0500

    Realigned Navbar on 1920 width, changed a word in Nightarticle, moved techGap between image and word in Projectbase closer together, and changed summary font weight to be lighter

--- src/app/components/nightArticle/NightArticle.tsx
+++ src/app/components/nightArticle/NightArticle.tsx
@@ -5,7 +5,7 @@ export default function NightArticle() {
   return (
     <section className={styles.main}>
       <p>The best way I find relaxation is through gaming, a hobby I&apos;ve cherished since childhood.</p>
-      <p>My gaming journey began with one of Blizzard&apos;s biggest games, StarCraft, and it has started my addiction ever since.</p>
+      <p>My gaming journey began with one of Blizzard&apos;s biggest games, StarCraft, and it has started my passion ever since.</p>
       <p>In times when gaming isn&apos;t an option, I turn to music for relaxation, particularly enjoying the songs by Infected Mushroom, one of my favorite musical duos.</p>
       <p>And if you ever need someone to join you in a game, don&apos;t hesitate to give me a shout!</p>
     </section>
